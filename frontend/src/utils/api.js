b71a03f77c7c9cdee1662cdfa1fdbb491f77de67
logitha-b <240059.ad@rmkec.ac.in>
2026-09-23 14:54:01 +0530

    Support VITE_API_URL environment variable for production deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '/api' })
 
 // Attach token to every request
 api.interceptors.request.use(config => {
